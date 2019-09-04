f0a3cab759a80157c989e5332923e568891a1f6a
William <william@monstertipp.com>
2019-09-04 16:24:51 +0200

    Add set_value

--- substrate-verifiable-credentials/runtime/src/verifiablecreds.rs
+++ substrate-verifiable-credentials/runtime/src/verifiablecreds.rs
@@ -1,15 +1,23 @@
-use support::{decl_storage, decl_module, StorageValue};
+use support::{decl_storage, decl_module, StorageValue, dispatch::Result};
+use system::ensure_signed;
 
 pub trait Trait: system::Trait + timestamp::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as VerifiableCreds {
-        SubjectCount: u32;
+        SubjectCount: u64;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
+        fn set_value(origin, value: u64) -> Result {
+            let _sender = ensure_signed(origin)?;
+
+            <SubjectCount<T>>::put(value);
+
+            Ok(())
+        }
     }
 }
